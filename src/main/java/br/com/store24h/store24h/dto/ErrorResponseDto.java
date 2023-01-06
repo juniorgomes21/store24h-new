25dc4f32f4fd4e806dcb24ea4829e8f9bb619039
Junior Gomes <69778545+juniorgomes21@users.noreply.github.com>
2023-01-05 22:31:02 -0300

    troca de senha implementada

--- src/main/java/br/com/store24h/store24h/dto/ErrorResponseDto.java
+++ src/main/java/br/com/store24h/store24h/dto/ErrorResponseDto.java
@@ -1,9 +1,20 @@
 package br.com.store24h.store24h.dto;
 
 public class ErrorResponseDto {
-    private String msg;
+    private String msg = "Ops, algo deu errado!";
+
+    public ErrorResponseDto() {
+    }
 
     public ErrorResponseDto(String msg) {
+        this.msg = msg;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
         this.msg = msg;
     }
 }
--- src/main/java/br/com/store24h/store24h/security/WebSecurityConfig.java
+++ src/main/java/br/com/store24h/store24h/security/WebSecurityConfig.java
@@ -88,7 +88,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/stubs/handler_api/comprarCredito").permitAll()
                 .antMatchers("/stubs/handler_api/getCredito").permitAll()
                 .antMatchers("/stubs/handler_api/getTableCredito").permitAll()
-
+                .antMatchers("/stubs/handler_api/edit/password").hasAuthority(Role.USER.getNome())
                 .antMatchers("/stubs/handler_api/getApiKey").hasAuthority(Role.USER.getNome())
                 //Padrão API CLIENTE
                 .antMatchers("/stubs/handler_api").permitAll()
