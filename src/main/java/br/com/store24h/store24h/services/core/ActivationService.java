8646ea7a11f15c6a48728ff4722cb4d6f7c66b41
Junior Gomes <69778545+juniorgomes21@users.noreply.github.com>
2023-01-29 02:21:11 -0300

    suporte para sms espeardo apenas

--- src/main/java/br/com/store24h/store24h/services/core/ActivationService.java
+++ src/main/java/br/com/store24h/store24h/services/core/ActivationService.java
@@ -2,7 +2,6 @@ package br.com.store24h.store24h.services.core;
 
 import br.com.store24h.store24h.model.Activation;
 import br.com.store24h.store24h.model.Servico;
-import br.com.store24h.store24h.model.SmsModel;
 import br.com.store24h.store24h.model.User;
 import br.com.store24h.store24h.repository.ActivationRepository;
 import br.com.store24h.store24h.repository.SmsRepository;
@@ -23,6 +22,9 @@ public class ActivationService {
     @Autowired
     private SmsRepository smsRepository;
 
+    @Autowired
+    private ServiceMapAlg serviceMapAlg;
+
     @Transactional
     public Long newActivation(User user, Servico servico, String chipNumber) {
         try {
@@ -30,7 +32,7 @@ public class ActivationService {
             compraService.buyService(user, servico, chipNumber);
             Activation activation = new Activation(servico.getName(), servico.getAlias(), chipNumber);
             activation.setStatusBuz(ActivationStatus.AGUARDANDO_MENSAGENS);
-            activation.setServiceNumber(servico.getAlias());
+//            activation.setServiceNumber(serviceMapAlg.getRegxByAlias(servico.getAlias()));
             smsRepository.deleteByChipnumber(chipNumber);
             return activationRepository.save(activation).getId();
         } catch (Exception e) {
--- src/main/java/br/com/store24h/store24h/services/core/ServiceMap.java
+++ src/main/java/br/com/store24h/store24h/services/core/ServiceMap.java
@@ -8,39 +8,78 @@ import java.util.HashMap;
 public class ServiceMap {
     private final HashMap<String, String> abrev = new HashMap<>(100) {{
 //        put("ul", "28153"); // universal  - 901692 é seu código de verificação do doar.universal.org.
-        put("dh", "0"); // eBay             -
+        //29117
+
+        put("wx", "0"); // apple
         put("eh", "0"); // Telegram 2."0"   -
-        put("fb", "0"); // Facebook         - 663220  :>4 4;O A1@>A0 20H53> ?0@>;O 2 «Facebook»
-        put("ig", "0"); // Instagram        -
-        put("ka", "0"); // Shopee           -
-        put("ki", "0"); // 99app            -
         put("lj", "0"); // Santander        -
-        put("mm", "0"); // Microsoft        -
-        put("mt", "0"); // Steam            -
         put("nf", "0"); // Netflix          -
-        put("sn", "0"); // Olx              -
-        put("tg", "0"); // Telegram         -
-        put("tn", "0"); // LinkedIN         -
         put("ts", "0"); // paypal           -
-        put("tw", "0"); // Twitter          -
-        put("ub", "0"); // Uber             -
-        put("uk", "0"); // Airbnb           -
-        put("vp", "0"); // Kwai             -
+
+        put("fb", "27100"); // Facebook         - 663220  :>4 4;O A1@>A0 20H53> ?0@>;O 2 «Facebook»
+        put("ka", "28149"); // Shopee           -
+        put("ki", "27100"); // 99app            -
+        put("sn", "28908"); // Olx              -
+        put("tg", "29222"); // Telegram         -
+        put("tn", "29454"); // LinkedIN         -
+
+        put("tw", "5655545156"); // Twitter          -
+        put("ub", "5655545156"); // Uber             -
+
+        put("mm", "29795"); // Microsoft        -  29415
+
+        put("vp", "29415"); // Kwai             -
+
+        put("uk", "29090"); // Airbnb           -
+
+                    //29415 - 29415 -29090
         put("wa", "29468"); // Whatsapp     - <#> Codigo do WhatsApp: 846-173
         put("??", "29468"); // Whatsapp B   - <#> Codigo do WhatsApp Business: 915-760 Se preferir, use este link para confirmar seu numero: b.whatsapp.com/915760 Nao compartilhe o codigo com
-        put("wx", "0"); // apple
-//        put("uk", "0"); // Airbnb
-//        put("uk", "0"); // Airbnb
-//        put("uk", "0"); // Airbnb
-//        put("uk", "0"); // Airbnb
+        put("ig", "29468"); // Instagram        -
+
+        put("kx", "103"); // Vivo
+        put("pd", "7370797968"); // iFood
+        put("am", "28060"); // Amazon
+        put("dh", "28060"); // eBay             -
+        put("mt", "28060"); // Steam            -
+        put("ds", "65858472778371"); // Discord
+
+    }};
+
+    private final HashMap<String, String> abrevReverse = new HashMap<>(100) {{
+//        put("ul", "28153"); // universal  - 901692 é seu código de verificação do doar.universal.org.
+        put("01", "dh"); // eBay             -
+        put("02", "eh"); // Telegram 2."0"   -
+        put("03", "fb"); // Facebook         - 663220  :>4 4;O A1@>A0 20H53> ?0@>;O 2 «Facebook»
+        put("04", "ig"); // Instagram        -
+        put("05", "ka"); // Shopee           -
+        put("06", "ki"); // 99app            -
+        put("07", "lj"); // Santander        -
+        put("08", "mm"); // Microsoft        -
+        put("09", "mt"); // Steam            -
+        put("a1", "nf"); // Netflix          -
+        put("a2", "sn"); // Olx              -
+        put("29222", "tg"); // Telegram         -
+        put("a3", "tn"); // LinkedIN         -
+        put("a4", "ts"); // paypal           -
+        put("a5", "tw"); // Twitter          -
+        put("a6", "ub"); // Uber             -
+        put("a7", "uk"); // Airbnb           -
+        put("a8", "vp"); // Kwai             -
+        put("wa", "29468"); // Whatsapp     - <#> Codigo do WhatsApp: 846-173
+        put("??", "29468"); // Whatsapp B   - <#> Codigo do WhatsApp Business: 915-760 Se preferir, use este link para confirmar seu numero: b.whatsapp.com/915760 Nao compartilhe o codigo com
+        put("a9", "wx"); // apple
 
     }};
 
     public ServiceMap(){
     }
 
-    public String getByAlias(String alias) {
+    public String getNumberByAlias(String alias) {
         return abrev.get(alias);
     }
 
+    public String getAliasByNumber(String serviceNumber) {
+        return abrevReverse.get(serviceNumber);
+    }
 }
